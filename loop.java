c777c8dfc6b7a073d6e0dbf6cdbeb92d6dbc2193
Sindhu-0-7 <sindhusakthi327@gmail.com>
2024-09-28 20:24:45 +0530

    adding all files

---
+++ loop.java
@@ -0,0 +1,9 @@
+class loop{
+    public static void main(String args[]){
+    int i=1;
+    for(i=1;i<=10;i=i+1)
+    {
+        System.out.println("john");
+    }
+}
+}
---
+++ loop2.java
@@ -0,0 +1,11 @@
+
+
+class loop2 {
+    public static void main(String[] args) {
+        int i=0;
+        for(i=1;i<=10;i++)
+    {
+        System.out.println(i);
+    }
+    }
+}
