e02d9538372b97a7aed116c977aeab1eff4d6fc2
Sindhu-0-7 <sindhusakthi327@gmail.com>
2024-10-03 18:42:09 +0530

    adding all files

---
+++ obj.java
@@ -0,0 +1,12 @@
+public class obj {
+    String name="hello";
+    String msg="world";
+    int time =23;
+public static void main(String[] args) {
+    obj obj1=new obj();
+    obj1.name="heyy";
+    System.out.println(obj1.name);
+    obj obj2=new obj();
+System.out.println(obj2.name);
+}
+}
